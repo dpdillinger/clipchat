3e3a884c2eb75090b75f40e3b9ba1a2b4a8172dc
Dan Dillinger <dpdillinger@gmail.com>
2011-09-04 11:19:36 -0400

    Added some read me descriptions.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clipchat "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure wrapper for HipChat API"
   :dependencies [[org.clojure/clojure "1.3.0-beta2"]
                  [org.clojure/data.json "0.1.1"]
                  [clj-http "0.1.3"]])

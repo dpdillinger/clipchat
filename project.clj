332eaa65b8e1050ca5d43f1fb5e36c0d2841991c
Dan Dillinger <dpdillinger@gmail.com>
2011-09-07 18:19:02 -0400

    updated to beta3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clipchat "1.0.0-SNAPSHOT"
   :description "Clojure wrapper for HipChat API"
-  :dependencies [[org.clojure/clojure "1.3.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
                  [org.clojure/data.json "0.1.1"]
                  [clj-http "0.1.3"]])

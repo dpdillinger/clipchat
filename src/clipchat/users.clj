48f19e48c52aff9a4541b75b57b75322da331884
Dan Dillinger <dpdillinger@gmail.com>
2011-09-07 18:20:23 -0400

    started work on implementing the rest of the users section of the api. fixed a stupid copy/paste/nochange. still can't decide if i like doing map defaults by destructuring or by a conj in the let binding more -- destructuring seems idiomatic, conj seems more readable and obvious

--- src/clipchat/users.clj
+++ src/clipchat/users.clj
@@ -9,9 +9,21 @@
                           is_group_admin :is_group_admin
                           password :password
                           timezone :timezone :as opts}]
-  (let [{:keys [room-id from message notify color] :or {is_group_admin 0 timezone "UTC" password ""}} opts
+  (let [{:keys [email name title is_group_admin password timezone] :or {is_group_admin 0 timezone "UTC" password ""}} opts
         url (str api-url "/users/create")
-        body (client/generate-query-string opts)]
+        body (client/generate-query-string (conj opts {:format "json" :auth_token auth-token}))]
+    (client/post url (setup-call-body body))))
+
+(defn update [auth-token {user_id :user_id
+                           email :email
+                           name :name
+                           title :title
+                           is_group_admin :is_group_admin
+                           password :password
+                           timezone :timezone :as opts}]
+  (let [{:keys [user_id email name title is_group_admin password timezone] :or {is_group_admin 0 timezone "UTC"}} opts
+        url (str api-url "/users/update")
+        body (client/generate-query-string (conj opts {:format "json" :auth_token auth-token}))]
     (client/post url (setup-call-body body))))
 
 (defn delete [auth-token {user_id :user_id :as opts}]
@@ -19,9 +31,15 @@
         body (assoc opts :auth_token auth-token)]
     (client/post url (setup-call-body body))))
 
-(defn list [auth-token])
+(defn list [auth-token]
+  (let [url (str api-url "/users/list")
+        query {:format "json" :auth_token auth-token}]
+    (client/get url {:query-params query})))
 
-(defn show [auth-token])
+(defn show [auth-token {user_id :user_id :as opts}]
+  (let [url (str api-url "/users/list")
+        query (conj opts {:format "json" :auth_token auth-token})]
+    (client/get url {:query-params query})))
 
-(defn update [auth-token])
+
 

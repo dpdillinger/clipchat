4aae7783eb429fe49cce24374e1272c451bf31b0
Daniel Dillinger <dan@Daniels-MacBook-Air.local>
2012-11-15 15:07:34 -0500

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clipchat "1.0.0-SNAPSHOT"
   :description "Clojure wrapper for HipChat API"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
-                 [clj-http "0.1.3"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.2.0"]
+                 [clj-http "0.5.8"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
